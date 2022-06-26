41545b5fb025e230bd74d06b5c2a8b849a634771
Riley Hunt <hunt4riley@csu.fullerton.edu>
2022-06-26 13:00:45 -0700

    Added navigation and data passing

--- DefectDispatch/Report.swift
+++ DefectDispatch/Report.swift
@@ -7,15 +7,47 @@
 
 import Foundation
 import CoreLocation
+import SwiftUI
 
-enum ReportType {
-    case pothole, suggestion, brokenLight;
+enum ReportType: String, CaseIterable, Identifiable {
+    static var allCases: [ReportType] {
+        return [.pothole, .suggestion, .brokenLight]
+    }
+    case pothole = "Pothole"
+    case suggestion = "Suggestion"
+    case brokenLight = "Broken Streetlight"
+    case undefined = "Select Report type"
+    var id: Self { self }
 }
 
-struct Report {
+enum Severity: String, CaseIterable, Identifiable {
+    static var allCases: [Severity] {
+        return [.high, .medium, .low]
+    }
+    case high = "High";
+    case medium = "Medium";
+    case low = "Low";
+    case unknown = "Estimate Severity";
+    var id: Self { self }
+}
+
+class Report: ObservableObject {
     var date: Date
-    var type: ReportType;
-    var severity: Int?
-    var details: String?
+    @Published var type: ReportType;
+    @Published var severity: Severity
+    @Published var details: String
     var location: CLLocationCoordinate2D?
+    var photo: Image?
+    
+    init() {
+        date = Date.now
+        type = ReportType.undefined
+        severity = Severity.unknown
+        details = ""
+    }
+    
+    func printType()
+    {
+        print(type.rawValue)
+    }
 }
--- DefectDispatch/ReportingInterface.swift
+++ DefectDispatch/ReportingInterface.swift
@@ -1,24 +1,123 @@
-import SwiftUI;
+import SwiftUI
 
-struct ReportingInterface : View {
+struct ReportingInterface: View {
+    @StateObject var report: Report = .init()
+    @State private var showingConfirmationPage: Bool = false;
+    @State private var showingErrorMessage: Bool = false;
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            // TODO: Add icons next to text
-            Button(action: {}) {
-                Text("Add Photo")
-            }
-            Button(action: {}) {
-                Text("Add Location")
-            }
-            Button(action: {}) {
-                Text("Estimate Severity")
+        GeometryReader { _ in
+            NavigationView {
+                VStack(alignment: .leading, spacing: 20) {
+                    HStack {
+                        NavigationLink(destination: ReportConfirmationPage().environmentObject(report), isActive: $showingConfirmationPage) {
+                            EmptyView()
+                        }
+                            .navigationBarBackButtonHidden(true)
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Button("Submit", action: {
+                                if report.severity != .unknown && report.type != .undefined
+                                {
+                                    showingConfirmationPage = true;
+                                } else
+                                {
+                                    showingErrorMessage = true
+                                }
+                            }).alert("Error!", isPresented: $showingErrorMessage, actions: {}, message: {
+                                Text("All reports must have a severity and a type!")
+                            })
+                            .modifier(RoundedButton(padding: 5, background: .green, cornerRadius: 40))
+                        }
+                    }
+                    Menu {
+                        Picker("Report Type", selection: $report.type, content: {
+                            ForEach(ReportType.allCases) { reportType in
+                                Text(reportType.rawValue)
+                            }
+                        })
+                    } label: {
+                        HStack
+                        {
+                            Text("Report Type:")
+                            Spacer()
+                            Text(report.type.rawValue)
+                        }.foregroundColor(.white)
+                            .modifier(RoundedButton(padding: 5, background: .blue, cornerRadius: 40))
+                    }
+                    HStack {
+                        Image(systemName: "camera.aperture")
+                        Text("Add Photo")
+                    }
+                    .modifier(RoundedButton(padding: 5, background: .blue, cornerRadius: 40))
+                    HStack {
+                        Image(systemName: "location.north.circle")
+                        Text("Add Location")
+                    }
+                    .modifier(RoundedButton(padding: 5, background: .blue, cornerRadius: 40))
+                    
+                    Menu {
+                        Picker("Severity Level", selection: $report.severity, content: {
+                            ForEach(Severity.allCases) { severity in
+                                Text(severity.rawValue.capitalized)
+                            }
+                        })
+                    } label: {
+                        HStack {
+                            Image(systemName: "exclamationmark.circle")
+                            Text(report.severity.rawValue.capitalized)
+                        }
+                        .modifier(SeverityButton(severity: $report.severity))
+                    }
+                    
+                    TextEditor(text: $report.details)
+                        .padding()
+                        .border(.gray, width: 2.0)
+                }.padding()
+                .navigationTitle("")
+                .navigationBarHidden(true)
             }
         }
     }
 }
 
-struct ReportingInterfaceUI_Previewa: PreviewProvider {
+struct RoundedButton: ViewModifier {
+    var padding: CGFloat;
+    var background: Color;
+    var cornerRadius: CGFloat;
+    func body(content: Content) -> some View {
+        content
+            .padding(padding)
+            .background(background)
+            .cornerRadius(cornerRadius)
+            .foregroundColor(.white)
+    }
+}
+
+struct ReportingInterfaceUI_Preview: PreviewProvider {
     static var previews: some View {
         ReportingInterface()
+    }
+}
+
+struct SeverityButton: ViewModifier {
+    @Binding var severity: Severity;
+    func body(content: Content) -> some View {
+        switch(severity)
+        {
+        case .high:
+            content
+                .modifier(RoundedButton(padding: 5, background: .red, cornerRadius: 40))
+        case .medium:
+            content
+                .modifier(RoundedButton(padding: 5, background: .yellow, cornerRadius: 40))
+                .foregroundColor(.black)
+        case .low:
+            content
+                .modifier(RoundedButton(padding: 5, background: .green, cornerRadius: 40))
+        default:
+            content
+                .modifier(RoundedButton(padding: 5, background: .gray, cornerRadius: 40))
+        }
     }
 }
