8a333d30c18936987f6d90a0761f4d9291822139
parrishstefan <stefannparrish@gmail.com>
2022-06-28 10:29:55 -0700

    - list added and persistent dark mode option

--- DefectDispatch/ReportList.swift
+++ DefectDispatch/ReportList.swift
@@ -2,7 +2,7 @@
 //  DefectList.swift
 //  DefectDispatch
 //
-//  Created by Jed Verry on 6/20/22.
+//  Created by Stefan Parrish
 //
 
 import Foundation
@@ -12,45 +12,128 @@ import SwiftUI
 
 struct ReportList: View {
     @EnvironmentObject var manager: ReportManager
+    @AppStorage("darkMode") var darkMode = 1;
+    
+    init() {
+        UITableView.appearance().backgroundColor = .clear
+    }
+    
     var body: some View {
         VStack {
-            Text("Defect Dispatch")
-                .bold()
-                .modifier(DefectDispatchText())
-            List {
-                ForEach(manager.reports) {
-                    reports in
-                    VStack (alignment: .leading) {
-                        Text(reports.name)
-                            .font(.largeTitle)
-                        Text(reports.description)
-                            .font(.caption)
-                    }
-                }
-                .onDelete {
-                    offset in
-                    manager.reports.remove(atOffsets: offset)
-                    ReportManager.save(manager)
-                }
-                .onMove {
-                    offset, index in
-                    manager.reports.move(fromOffsets: offset,
-                                            toOffset: index)
-                    ReportManager.save(manager)
-                }
+            HStack {
+                Text("Defect Dispatch")
+                    .bold()
+                    .modifier(DefectDispatchText())
+                Button(action: {
+                    darkMode = (darkMode-1)*(-1)
+                }, label: {
+                    Text("💡")
+                })
             }
+            NavigationView {
+                List {
+                    ForEach(manager.reports) { reports in
+                        NavigationLink(destination: ReportView(description: reports.description, title: reports.name, address: reports.address)) {
+                            VStack (alignment: .leading) {
+                                Text(reports.name)
+                                    .font(.largeTitle)
+                                Text(reports.description)
+                                    .font(.caption)
+                            }
+                        }.modifier(listModifier())
+                    }
+                    .onDelete {
+                        offset in
+                        manager.reports.remove(atOffsets: offset)
+                        ReportManager.save(manager)
+                    }
+                    .onMove {
+                        offset, index in
+                        manager.reports.move(fromOffsets: offset,
+                                                toOffset: index)
+                        ReportManager.save(manager)
+                    }
+                }.modifier(darkModeReport())
+            }
+        } .modifier(darkModeReport())
+    }
+}
+
+struct ReportView: View {
+    @State var description = "";
+    @State var title = "";
+    @State var address = "";
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                VStack {
+                    Text(title)
+                        .font(.largeTitle)
+                    Image("StopStreet")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width:250, height: 250)
+                }
+                .frame(width: geometry.size.width, height:geometry.size.height/2)
+                VStack {
+                    Text(description)
+                        .font(.title2)
+                    Text("Located at: \(address)")
+                        .font(.subheadline)
+                        .padding()
+                }
+                .frame(width: geometry.size.width, height:geometry.size.height/2)
+            }.modifier(darkModeReport())
         }
     }
 }
 
 struct DefectDispatchText: ViewModifier {
+    @AppStorage("darkMode") var darkMode = 1;
    func body(content: Content) -> some View {
-        content
-           .font(.custom("Courier New", size: 20))
-           .foregroundColor(Color.white)
-           .padding()
-           .background(Color.black)
-           .cornerRadius(10)
+       if(darkMode == 1) {
+            content
+               .font(.custom("Courier New", size: 20))
+               .foregroundColor(Color.white)
+               .padding()
+               .background(Color.black)
+       } else {
+           content
+              .font(.custom("Courier New", size: 20))
+              .foregroundColor(Color.black)
+              .padding()
+              .background(Color.white)
+       }
+    }
+}
+
+struct darkModeReport: ViewModifier {
+    @AppStorage("darkMode") var darkMode = 1;
+    func body(content: Content) -> some View {
+        if(darkMode == 1) {
+            content
+                .background(Color.black)
+                .foregroundColor(Color.white)
+        } else {
+            content
+                .background(Color.white)
+                .foregroundColor(Color.black)
+        }
+    }
+}
+
+struct listModifier: ViewModifier {
+    @AppStorage("darkMode") var darkMode = 1;
+    func body(content: Content) -> some View {
+        if(darkMode == 1) {
+            content
+                .listRowBackground(Color("DarkGray"))
+                .foregroundColor(Color.white)
+        } else {
+            content
+                .listRowBackground(Color.white)
+                .foregroundColor(Color.black)
+        }
     }
 }
 
